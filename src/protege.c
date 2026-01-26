53cec5cc5b87c1b1665e588933b845a73df67bac
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2026-01-26 20:43:58 +0000

    Prepare 0.3.4 release.
    Bump version number and copyright notices.

--- src/protege.c
+++ src/protege.c
@@ -1,6 +1,6 @@
 /*
  * Protégé launcher
- * Copyright (C) 2022 Damien Goutte-Gattat
+ * Copyright (C) 2022,2026 Damien Goutte-Gattat
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
